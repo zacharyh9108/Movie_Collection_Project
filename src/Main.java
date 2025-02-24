ee23ca903f22508af39e8d145a11a27ea558bd35
zacharyh9108 <zacharyhodge08@gmail.com>
2025-02-24 10:58:52 -0500

    Create Movie Collection Project

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MovieCollection myCollection = new MovieCollection("src/movies_data.csv");
+        myCollection.menu();
+    }
+}
